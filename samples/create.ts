800d83bb6df926ece70f3a5ae5135c4e90c34b49
0-micon <0.miconst@gmail.com>
2021-01-17 15:17:25 +0300

    + createBatman function

--- samples/create.ts
+++ samples/create.ts
@@ -1,4 +1,5 @@
 import * as SPD from "svg-path-d";
+import { createTransformed } from "svg-path-d/dist/transform";
 
 export function createCircle(
   centerX: number,
@@ -148,4 +149,35 @@ export function createRing(
   // To cut a hole we should reverse the hole path.
   const hole = SPD.createReveresed(createCircle(centerX, centerY, rMin));
   return SPD.makePath(ring.concat(hole));
+}
+
+export function createBatman(
+  centerX: number,
+  centerY: number,
+  radius: number
+): SPD.PathNode[] {
+  const r0 = 500;
+  const logo =
+    "M0-208h28l30-36 14 136c26 137 215 46 88-137a447 263 0 01109 455" +
+    "c84-117-60-160-128-30-37-113-121-27-141 52-21-80-103-166-141-51-48-112-212-112-129 29" +
+    "a447 263 0 01109-455c-123 180 59 277 89 137l14-136 30 36h28z";
+
+  let path = SPD.fromString(logo);
+  if (radius && radius !== r0) {
+    const ms = SPD.utils.matrix.createScale(radius / r0, radius / r0);
+    path = createTransformed(path, ms);
+  }
+
+  // Add an oval.
+  let ring = createTransformed(
+    createRing(0, 0, radius * 0.925, radius),
+    SPD.utils.matrix.createScale(1, 0.6125)
+  );
+  path = SPD.makePath(ring.concat(path));
+
+  if (centerX || centerY) {
+    const mt = SPD.utils.matrix.createTranslate(centerX, centerY);
+    path = createTransformed(path, mt);
+  }
+  return path;
 }
